b9780926cb9941a3b7d42476103c2b67d4a1bb7c
XXbq567 <wbl2855873334@gmail.com>
2025-08-08 00:23:35 +1000

    Update tray.cpp

--- src/tray.cpp
+++ src/tray.cpp
@@ -1,47 +1,38 @@
-// tray.cpp
 #include "pch.h"
-#include "resource.h"
+#include "../res/resource.h"
 
-#define WM_TRAY (WM_USER+1)
-NOTIFYICONDATAW nid{};
+#define WM_TRAY (WM_USER + 1)
 
-bool RegisterTray(HWND hWnd)
-{
-    nid.cbSize           = sizeof(nid);
-    nid.hWnd             = hWnd;
-    nid.uID              = 100;
-    nid.uFlags           = NIF_ICON|NIF_MESSAGE|NIF_TIP;
+extern void ToggleDesktopIcons();
+extern void ShowSettingsDlg(HWND);
+
+bool RegisterTray(HWND hWnd) {
+    NOTIFYICONDATAW nid{ sizeof(nid) };
+    nid.hWnd = hWnd;
+    nid.uID = 100;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
     nid.uCallbackMessage = WM_TRAY;
-    nid.hIcon            = LoadIcon(GetModuleHandle(nullptr),
-                                    MAKEINTRESOURCE(IDI_ICON1));
+    nid.hIcon = LoadIcon(GetModuleHandle(nullptr), MAKEINTRESOURCE(IDI_ICON1));
     wcscpy_s(nid.szTip, L"桌面图标控制器");
+    Shell_NotifyIconW(NIM_ADD, &nid);
 
-    Shell_NotifyIconW(NIM_ADD,&nid);
-
-    HMENU hMenu = CreatePopupMenu();
-    AppendMenuW(hMenu, MF_STRING, 1000, L"设置");
-    AppendMenuW(hMenu, MF_STRING, 1001, L"退出");
-
-    SetWindowLongPtrW(hWnd, GWLP_WNDPROC, (LONG_PTR)
-        [](HWND h, UINT m, WPARAM w, LPARAM l)->LRESULT{
-            if(m==WM_TRAY && l==WM_RBUTTONUP){
+    SetWindowLongPtrW(hWnd, GWLP_WNDPROC,
+        (LONG_PTR)[](HWND h, UINT m, WPARAM w, LPARAM l)->LRESULT {
+            if (m == WM_TRAY && l == WM_RBUTTONUP) {
                 POINT pt; GetCursorPos(&pt);
-                HMENU hMenu = CreatePopupMenu();
-                AppendMenuW(hMenu, MF_STRING, 1000, L"设置");
-                AppendMenuW(hMenu, MF_STRING, 1001, L"退出");
+                HMENU menu = CreatePopupMenu();
+                AppendMenuW(menu, MF_STRING, 1000, L"设置");
+                AppendMenuW(menu, MF_STRING, 1001, L"退出");
                 SetForegroundWindow(h);
-                TrackPopupMenu(hMenu, TPM_RIGHTBUTTON, pt.x,pt.y,0,h,nullptr);
-                DestroyMenu(hMenu);
+                TrackPopupMenu(menu, TPM_RIGHTBUTTON, pt.x, pt.y, 0, h, nullptr);
+                DestroyMenu(menu);
             }
-            if(m==WM_COMMAND){
-                if(LOWORD(w)==1000) ShowSettingsDlg(h);
-                if(LOWORD(w)==1001){ Shell_NotifyIconW(NIM_DELETE,&nid); PostQuitMessage(0); }
+            if (m == WM_COMMAND) {
+                if (LOWORD(w) == 1000) ShowSettingsDlg(h);
+                if (LOWORD(w) == 1001) { Shell_NotifyIconW(NIM_DELETE, &nid); PostQuitMessage(0); }
             }
-            if(m==WM_HOTKEY && w==1){
-                extern void ToggleDesktopIcons();
-                ToggleDesktopIcons();
-            }
-            return DefWindowProc(h,m,w,l);
+            if (m == WM_HOTKEY && w == 1) ToggleDesktopIcons();
+            return DefWindowProc(h, m, w, l);
         });
     return true;
 }
