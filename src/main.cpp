21b73767e94c8bcf97c340ad932fb3ac5d4ff92c
XXbq567 <wbl2855873334@gmail.com>
2025-08-08 00:22:46 +1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,31 @@
-// main.cpp
 #include "pch.h"
-#include "resource.h"
+#include "../res/resource.h"
 
-extern bool RegisterTray(HWND hWnd);
-extern void ShowSettingsDlg(HWND hParent);
+extern bool RegisterTray(HWND);
+extern void ShowSettingsDlg(HWND);
 
-int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int)
-{
+int WINAPI wWinMain(HINSTANCE h, HINSTANCE, PWSTR, int) {
     InitCommonControls();
     CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
-    WNDCLASSW wc{0};
-    wc.lpfnWndProc   = [](HWND h, UINT m, WPARAM w, LPARAM l)->LRESULT{
-        if(m==WM_DESTROY){ PostQuitMessage(0); return 0; }
-        return DefWindowProc(h,m,w,l);
-    };
-    wc.hInstance     = hInst;
+    WNDCLASSW wc{ 0 };
+    wc.lpfnWndProc = DefWindowProcW;
+    wc.hInstance = h;
     wc.lpszClassName = L"DeskIconCtlCls";
     RegisterClassW(&wc);
 
-    HWND hWnd = CreateWindowW(wc.lpszClassName, L"", 0, 0,0,0,0,
-                              HWND_MESSAGE, nullptr, hInst, nullptr);
+    HWND hwnd = CreateWindowW(wc.lpszClassName, L"", 0, 0, 0, 0, 0,
+                              HWND_MESSAGE, nullptr, h, nullptr);
 
-    RegisterHotKey(hWnd, 1, MOD_CONTROL|MOD_SHIFT, 'D');
-    RegisterTray(hWnd);
+    RegisterHotKey(hwnd, 1, MOD_CONTROL | MOD_SHIFT, 'D');
+    RegisterTray(hwnd);
 
     MSG msg;
-    while(GetMessageW(&msg,nullptr,0,0)){
+    while (GetMessageW(&msg, nullptr, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessageW(&msg);
     }
-    UnregisterHotKey(hWnd,1);
+    UnregisterHotKey(hwnd, 1);
     CoUninitialize();
     return 0;
 }
