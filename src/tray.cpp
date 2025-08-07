2ace2cc5e8084b299e1e21093365c05bf88c79a4
XXbq567 <wbl2855873334@gmail.com>
2025-08-08 01:34:00 +1000

    Update tray.cpp

--- src/tray.cpp
+++ src/tray.cpp
@@ -16,23 +16,28 @@ bool RegisterTray(HWND hWnd) {
     wcscpy_s(nid.szTip, L"桌面图标控制器");
     Shell_NotifyIconW(NIM_ADD, &nid);
 
+    auto newWndProc = [](HWND h, UINT m, WPARAM w, LPARAM l,
+                         NOTIFYICONDATAW nidLocal)->LRESULT {
+        if (m == WM_TRAY && l == WM_RBUTTONUP) {
+            POINT pt; GetCursorPos(&pt);
+            HMENU menu = CreatePopupMenu();
+            AppendMenuW(menu, MF_STRING, 1000, L"设置");
+            AppendMenuW(menu, MF_STRING, 1001, L"退出");
+            SetForegroundWindow(h);
+            TrackPopupMenu(menu, TPM_RIGHTBUTTON, pt.x, pt.y, 0, h, nullptr);
+            DestroyMenu(menu);
+        }
+        if (m == WM_COMMAND) {
+            if (LOWORD(w) == 1000) ShowSettingsDlg(h);
+            if (LOWORD(w) == 1001) { Shell_NotifyIconW(NIM_DELETE, &nidLocal); PostQuitMessage(0); }
+        }
+        if (m == WM_HOTKEY && w == 1) ToggleDesktopIcons();
+        return DefWindowProc(h, m, w, l);
+    };
+
     SetWindowLongPtrW(hWnd, GWLP_WNDPROC,
         (LONG_PTR)[](HWND h, UINT m, WPARAM w, LPARAM l)->LRESULT {
-            if (m == WM_TRAY && l == WM_RBUTTONUP) {
-                POINT pt; GetCursorPos(&pt);
-                HMENU menu = CreatePopupMenu();
-                AppendMenuW(menu, MF_STRING, 1000, L"设置");
-                AppendMenuW(menu, MF_STRING, 1001, L"退出");
-                SetForegroundWindow(h);
-                TrackPopupMenu(menu, TPM_RIGHTBUTTON, pt.x, pt.y, 0, h, nullptr);
-                DestroyMenu(menu);
-            }
-            if (m == WM_COMMAND) {
-                if (LOWORD(w) == 1000) ShowSettingsDlg(h);
-                if (LOWORD(w) == 1001) { Shell_NotifyIconW(NIM_DELETE, &nid); PostQuitMessage(0); }
-            }
-            if (m == WM_HOTKEY && w == 1) ToggleDesktopIcons();
-            return DefWindowProc(h, m, w, l);
+            return newWndProc(h, m, w, l, nid);
         });
     return true;
 }
