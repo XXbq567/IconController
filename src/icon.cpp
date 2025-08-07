06b3077e884ce666c41ecfc6952872a6eec48d4b
XXbq567 <wbl2855873334@gmail.com>
2025-08-08 00:23:46 +1000

    Update icon.cpp

--- src/icon.cpp
+++ src/icon.cpp
@@ -1,32 +1,26 @@
-// icon.cpp
 #include "pch.h"
 
-void ToggleDesktopIcons()
-{
-    IShellWindows* psw=nullptr;
-    CoCreateInstance(CLSID_ShellWindows,nullptr,CLSCTX_ALL,
-                     IID_IShellWindows,(void**)&psw);
-    if(!psw) return;
+void ToggleDesktopIcons() {
+    IShellWindows* psw = nullptr;
+    CoCreateInstance(CLSID_ShellWindows, nullptr, CLSCTX_ALL, IID_IShellWindows, (void**)&psw);
+    if (!psw) return;
 
-    IDispatch* pdisp=nullptr;
-    VARIANT vLoc; vLoc.vt=VT_I4; vLoc.lVal=CSIDL_DESKTOP;
-    if(SUCCEEDED(psw->FindWindowSW(&vLoc, nullptr, SWC_DESKTOP,
-                                    (long*)(void*)&pdisp, SWFO_NEEDDISPATCH)))
-    {
-        IServiceProvider* psp=nullptr;
-        if(SUCCEEDED(pdisp->QueryInterface(IID_IServiceProvider,(void**)&psp))){
-            IShellBrowser* psb=nullptr;
-            if(SUCCEEDED(psp->QueryService(SID_STopLevelBrowser,
-                                           IID_IShellBrowser,(void**)&psb))){
-                IShellView* psv=nullptr;
-                if(SUCCEEDED(psb->QueryActiveShellView(&psv))){
-                    IFolderView2* pfv=nullptr;
-                    if(SUCCEEDED(psv->QueryInterface(IID_IFolderView2,
-                                                     (void**)&pfv))){
-                        DWORD flags=0;
+    IDispatch* pdisp = nullptr;
+    VARIANT v{ VT_I4, CSIDL_DESKTOP };
+    if (SUCCEEDED(psw->FindWindowSW(&v, nullptr, SWC_DESKTOP,
+                                     (long*)(void*)&pdisp, SWFO_NEEDDISPATCH))) {
+        IServiceProvider* psp = nullptr;
+        if (SUCCEEDED(pdisp->QueryInterface(IID_IServiceProvider, (void**)&psp))) {
+            IShellBrowser* psb = nullptr;
+            if (SUCCEEDED(psp->QueryService(SID_STopLevelBrowser, IID_IShellBrowser, (void**)&psb))) {
+                IShellView* psv = nullptr;
+                if (SUCCEEDED(psb->QueryActiveShellView(&psv))) {
+                    IFolderView2* pfv = nullptr;
+                    if (SUCCEEDED(psv->QueryInterface(IID_IFolderView2, (void**)&pfv))) {
+                        DWORD flags = 0;
                         pfv->GetCurrentFolderFlags(&flags);
                         pfv->SetCurrentFolderFlags(FWF_NOICONS,
-                                                   flags&FWF_NOICONS ? 0 : FWF_NOICONS);
+                                                   flags & FWF_NOICONS ? 0 : FWF_NOICONS);
                         pfv->Release();
                     }
                     psv->Release();
