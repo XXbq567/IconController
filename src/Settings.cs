c1a2c18aa2daa37c5021c77823e8bb2095e0b547
XXbq567 <wbl2855873334@gmail.com>
2025-08-09 22:58:56 +1000

    Update Settings.cs

--- src/Settings.cs
+++ src/Settings.cs
@@ -1,31 +1,36 @@
+using System;
+using System.IO;
 using System.Text.Json;
 
-public class Settings
+namespace DesktopToggle
 {
-    public string Hotkey { get; set; } = "Ctrl+Alt+H";
-    public bool AutoStart { get; set; } = false;
-    public bool ShowTrayIcon { get; set; } = true;
-    public bool Enabled { get; set; } = true;
-
-    private static readonly string Path = System.IO.Path.Combine(
-        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-        "DesktopToggle", "settings.json");
-
-    public void Save()
+    public class Settings
     {
-        Directory.CreateDirectory(System.IO.Path.GetDirectoryName(Path)!);
-        File.WriteAllText(Path, JsonSerializer.Serialize(this));
-    }
+        public string Hotkey { get; set; } = "Ctrl+Alt+H";
+        public bool AutoStart { get; set; } = false;
+        public bool ShowTrayIcon { get; set; } = true;
+        public bool Enabled { get; set; } = true;
 
-    public static Settings Load()
-    {
-        try
+        private static readonly string Path = System.IO.Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "DesktopToggle", "settings.json");
+
+        public void Save()
         {
-            return JsonSerializer.Deserialize<Settings>(File.ReadAllText(Path)) ?? new Settings();
+            Directory.CreateDirectory(System.IO.Path.GetDirectoryName(Path)!);
+            File.WriteAllText(Path, JsonSerializer.Serialize(this));
         }
-        catch
+
+        public static Settings Load()
         {
-            return new Settings();
+            try
+            {
+                return JsonSerializer.Deserialize<Settings>(File.ReadAllText(Path)) ?? new Settings();
+            }
+            catch
+            {
+                return new Settings();
+            }
         }
     }
 }
