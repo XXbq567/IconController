7fc14b061b3b9b366c63f57a8299d28d67c12bd4
XXbq567 <wbl2855873334@gmail.com>
2025-08-09 22:04:39 +1000

    Create Settings.cs

---
+++ src/Settings.cs
@@ -0,0 +1,31 @@
+using System.Text.Json;
+
+public class Settings
+{
+    public string Hotkey { get; set; } = "Ctrl+Alt+H";
+    public bool AutoStart { get; set; } = false;
+    public bool ShowTrayIcon { get; set; } = true;
+    public bool Enabled { get; set; } = true;
+
+    private static readonly string Path = System.IO.Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+        "DesktopToggle", "settings.json");
+
+    public void Save()
+    {
+        Directory.CreateDirectory(System.IO.Path.GetDirectoryName(Path)!);
+        File.WriteAllText(Path, JsonSerializer.Serialize(this));
+    }
+
+    public static Settings Load()
+    {
+        try
+        {
+            return JsonSerializer.Deserialize<Settings>(File.ReadAllText(Path)) ?? new Settings();
+        }
+        catch
+        {
+            return new Settings();
+        }
+    }
+}
