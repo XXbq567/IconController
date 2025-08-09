5de9f9702008a226aad346e7ab803c748eb6f6ea
XXbq567 <wbl2855873334@gmail.com>
2025-08-09 21:59:50 +1000

    Delete src directory

--- src/pch.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <windows.h>
-#include <commctrl.h>
-#include <shellapi.h>
-#include <shlobj.h>
-#include <shobjidl.h>
-#include <string>
-#pragma comment(lib, "shell32.lib")
-#pragma comment(lib, "comctl32.lib")
